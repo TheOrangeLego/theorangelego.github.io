cb67f35600943312afca0811f27bb2f841725155
theorangelego <gzjosef@gmail.com>
2020-06-29 00:37:32 -0700

    Initial commit

---
+++ src/Application.tsx
@@ -0,0 +1,13 @@
+import { registerRootComponent } from 'expo'
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Application = () => (
+  <>
+    <View>
+      <Text>Hello World!! -- SITE UNDER CONSTRUCTION</Text>
+    </View>
+  </>
+)
+
+export default registerRootComponent( Application )
