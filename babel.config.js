768184e2dbb085ca6010507a5485ac33716e1e58
theorangelego <gzjosef@gmail.com>
2020-07-01 20:51:05 -0700

    Testing with router navigation

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function( api ) {
+  api.cache( true )
   return {
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '~': 'src'
+          },
+          extensions: ['.ts', '.tsx'],
+          root: ['src']
+        }
+      ]
+    ],
     presets: ['babel-preset-expo'],
-  };
-};
+  }
+}
--- src/Application.tsx
+++ src/Application.tsx
@@ -1,13 +1,33 @@
 import { registerRootComponent } from 'expo'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import { styles } from 'styles'
+import Homepage from 'homepage'
+import { Button } from 'components'
+
+const homepageLink = '/'
+const guidesLink   = '/guides'
+const papersLink   = '/papers'
+const projectsLink = '/projects'
 
 const Application = () => (
-  <>
-    <View>
-      <Text>SITE UNDER CONSTRUCTION</Text>
-    </View>
-  </>
+  <View style={styles.rootContainer}>
+    <BrowserRouter>
+      <View style={styles.headerContainer}>
+        <View style={styles.buttonContainer}>
+          <Button link={homepageLink} title={"homepage"} />
+          <Button link={guidesLink} title={"guides"} />
+          <Button link={papersLink} title={"papers"} />
+          <Button link={projectsLink} title={"projects"} />
+        </View>
+      </View>
+      <Switch>
+        <Route path={homepageLink} component={Homepage} exact />
+      </Switch>
+    </BrowserRouter>
+  </View>
 )
 
 export default registerRootComponent( Application )
